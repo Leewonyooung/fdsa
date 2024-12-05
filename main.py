8aa8dc88bce378a31372b7105aa79d3e945ac111
Leewonyoung <wylee99@naver.com>
2024-12-05 18:45:18 +0900

    sss

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+app.mount("/", StaticFiles(directory="web", html=True), name="web")
+
+if __name__ == "main":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
