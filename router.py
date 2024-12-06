8d1a9438b6282f7d4b5daa263a089d6f2953b6fb
Leewonyoung <wylee99@naver.com>
2024-12-06 11:23:20 +0900

    asdf

--- router.py
+++ router.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
-import pymysql, hosts
 from fastapi.middleware.cors import CORSMiddleware
-from auth import router as auth_router
-from rent import router as rent_router
-from station import router as station_router
-from login import router as login_router
-from reservation import router as reservation_router
-from API_KEY import URL
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -17,26 +11,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-def connect():
-    conn = pymysql.connect(
-        host=hosts.bicycle,
-        user='root',
-        password='qwer1234',
-        charset='utf8',
-        db='bicycle',
-        port=32176
-    )
-    return conn
-
-
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(rent_router, prefix="/rent", tags=["rent"])
-app.include_router(station_router, prefix="/station", tags=["station"])
-app.include_router(login_router, prefix="/login", tags=["login"])
-app.include_router(reservation_router, prefix="/reserve", tags=["reserve"])
-
-
+app.mount("/", StaticFiles(directory="web", html=True), name="web")
 
 
 if __name__ == "__main__":
